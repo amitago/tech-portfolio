8163fd1908a1a2942df756f09751902116002a13
amitago <61812948+amitago@users.noreply.github.com>
2020-10-02 15:24:50 -0400

    I started my technical portfolio

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
-    profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    title: 'Amita Goyal', // Title for your website.
+    tagline: 'Perpetual learner with a love for coding, history, and literature.',
+    profilePhoto: 'img/ID Photo.jpg',
+    url: 'https://amitago.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'amitago',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'amita-goyal-a23697172',
         twitter: '',
-        github: '',
+        github: 'amitago',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
